82e4c2dc9d5c7e6aa3287b3326758ada9152dcbd
Dante Campregher <dante.campregher@student.uva.nl>
2022-12-08 15:25:22 +0100

    cleaned 8_2 a bit

--- Day 8/day8_2.py
+++ Day 8/day8_2.py
@@ -6,19 +6,15 @@ width = len(grid[0])
 height = len(grid)
 count = height * width
 scores = []
-print(height, width)
 
 for i, row in enumerate(grid[1:-1]):
     for j, cell in enumerate(row[1:-1]):
         a, b, c, d = 1, 1, 1, 1
 
         dx = 1
-        try:
-            while j+1+dx < width-1 and grid[i+1][j+1+dx] < cell:
-                dx += 1
-                a += 1
-        except IndexError:
-            print(i, j, dx)
+        while j+1+dx < width-1 and grid[i+1][j+1+dx] < cell:
+            dx += 1
+            a += 1
         dx = -1
         while j+1+dx > 0 and grid[i+1][j+1+dx] < cell:
             dx -= 1
